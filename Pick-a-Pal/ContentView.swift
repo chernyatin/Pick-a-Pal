77be74f32bad7b7138361cb5939ff4b2ead09b59
Denis Chernyatin <ya.chernyatin@gmail.com>
2023-11-20 16:50:54 +0300

    Initial Commit

---
+++ Pick-a-Pal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pick-a-Pal
+//
+//  Created by Денис Чернятин on 2023.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Денис Чернятин on 2023.11.20.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
