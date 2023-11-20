4ce924faa7495fc7920e006dc91d72e68a038883
Denis Chernyatin <ya.chernyatin@gmail.com>
2023-11-20 21:29:13 +0300

    MVP

--- Pick-a-Pal/ContentView.swift
+++ Pick-a-Pal/ContentView.swift
@@ -8,12 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var names: [String] = []
+    @State private var nameToAdd = ""
+    @State private var pickedName = ""
+    @State private var shouldRemovePickedName = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+            VStack(spacing: 8) {
+                Image(systemName: "person.3.sequence.fill")
+                    .foregroundStyle(.tint)
+                    .symbolRenderingMode(.hierarchical)
+                Text("Кто Бежит За Клинским?!")
+                    .font(.title)
+                    .bold()
+            }
+            
+            Text(pickedName.isEmpty ? " " : pickedName)
+                .font(.title2)
+                .bold()
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            List {
+                ForEach(names, id: \.self) { name in Text(name)
+                }
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+            
+            TextField("Добавь Имя Собутыльника", text: $nameToAdd)
+                .autocorrectionDisabled()
+                .onSubmit {
+                    if !nameToAdd.isEmpty {
+                        names.append(nameToAdd)
+                        nameToAdd = ""
+                    }
+                }
+            Toggle("Режим Последнего Выжившего", isOn: $shouldRemovePickedName)
+            
+            Button {
+                if names.count > 0 {
+                    pickedName = names.randomElement()!
+                    if shouldRemovePickedName {
+                        names.removeAll() { name in
+                            return (name == pickedName)
+                        }
+                    }
+                } else {
+                    pickedName = ""
+                }
+            } label: {
+                Text("Выбрать Случайное Имя")
+                    .padding(.vertical, 8)
+                    .padding(.horizontal, 16)
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
         }
         .padding()
     }
